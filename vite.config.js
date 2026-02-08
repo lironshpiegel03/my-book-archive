aa11e4bbb697542d3b1b63e203983d4660f26e8f
lironshpiegel03 <liron200307@gmail.com>
2026-02-08 14:49:35 +0200

    Fix base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,4 +6,3 @@ export default defineConfig({
   plugins: [react()],
   base: "/my-book-archive/",
 });
-
